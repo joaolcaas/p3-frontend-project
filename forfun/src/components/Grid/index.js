8ece1163a37731dbaad9cd3ee52f8ea3a69f1dd5
JOAO LUCAS ARAUJO FELIPE 114211023 <joaolaf@lcc3-107.lcc.ufcg.edu.br>
2018-11-21 17:37:35 -0300

    requisição get

--- forfun/src/components/Grid/index.js
+++ forfun/src/components/Grid/index.js
@@ -2,9 +2,23 @@ import React,{Component} from "react";
 import { Grid } from "semantic-ui-react";
 import GameCard from "../Card/index";
 import "./index.css"
+import axios from 'axios';
 
 
 export default class GameGrid extends Component{
+
+    state = {
+        gamesCards:[]
+    }
+
+    componentDidMount(){
+        axios.get('http://localhost:3000/game')
+        .then(res=>{
+            const gameCards = res.data
+            this.setState({gameCards});
+        })
+    }
+
     render(){
         const { games } = this.props;
         
